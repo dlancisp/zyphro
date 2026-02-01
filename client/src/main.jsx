9e90c00e9cd2a0fd7131b382fc4b567aac600db6
david <davidlancisdepaula@gmail.com>
2026-02-01 13:28:17 +0100

    Fix: Añadir BrowserRouter en main.jsx y dependencia

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom' // <--- 1. IMPORTAR LIBRERÍA
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* <--- 2. ENVOLVER LA APP */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
