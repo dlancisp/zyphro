3fefdeaa344f9986a56264d438eed5ae22bda31b
david <davidlancisdepaula@gmail.com>
2026-02-01 12:33:03 +0100

    Arreglando CORS para Vercel

--- server/index.js
+++ server/index.js
@@ -11,7 +11,15 @@ const prisma = new PrismaClient(); // Iniciamos la conexión
 // --- MIDDLEWARES DE SEGURIDAD ---
 app.use(helmet());
 app.use(express.json({ limit: '10kb' }));
-app.use(cors({ origin: process.env.CLIENT_URL || '*' })); // Ajustar en prod
+app.use(cors({
+  origin: [
+    "http://localhost:5173",             // Tu PC
+    "https://zyph-v1.vercel.app",        // Tu Web en Vercel (Pega AQUÍ tu dominio exacto)
+    "https://zyph-suite.vercel.app"      // (Si tienes otro dominio, ponlo también)
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 const limiter = rateLimit({ windowMs: 15*60*1000, max: 100 });
 app.use('/api', limiter);
